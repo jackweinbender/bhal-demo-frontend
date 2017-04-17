960bfa094f9fc11700e2a6eea26f8ab950b9e379
Jack Weinbender <jack.weinbender@gmail.com>
2017-04-16 22:23:17 -0500

    CSP?

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,9 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.APP.API_HOST = 'http://localhost:8000';
+    ENV.contentSecurityPolicy = {
+      'connect-src': "'self' http://localhost:8000"
+    }
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
